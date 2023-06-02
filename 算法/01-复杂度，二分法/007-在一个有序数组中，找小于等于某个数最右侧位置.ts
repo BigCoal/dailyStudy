05c98052973562cd59dd9bf7717eefef149652a9
dingtalk_dtgfuk <1048506792@qq.com>
2023-06-02 16:45:39 +0800

    【perf】优化文件名

--- 算法/01-复杂度，二分法/007-在一个有序数组中，找小于等于某个数最右侧位置.ts
+++ 算法/01-复杂度，二分法/007-在一个有序数组中，找小于等于某个数最右侧位置.ts
@@ -1,15 +1,15 @@
 //>=3的最左侧位置
-function nearestIndex(arr, num) {
+function nearestIndex(arr: number[], num: number) {
   let L = 0;
   let R = arr.length - 1;
   let index = -1;
   while (L <= R) {
     const mid = L + ((R - L) >> 1);
-    if (arr[mid] >= num) {
+    if (arr[mid] <= num) {
       index = mid;
-      R = mid - 1;
-    } else {
       L = mid + 1;
+    } else {
+      R = mid - 1;
     }
   }
   return index;
